322e96f5265709a59c1874d091c66a5adb0447a8
Lee-YongJun <0311odi@gmail.com>
2022-01-27 14:11:26 +0900

    변경

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 public class RequestParamServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        System.out.println("변경");
         System.out.println("[전체 파라미터 조회]-start");
         request.getParameterNames().asIterator().forEachRemaining(paramName -> System.out.println(paramName + " = " + request.getParameter(paramName)));
         System.out.println("[전체 파라미터 조회]-end");
